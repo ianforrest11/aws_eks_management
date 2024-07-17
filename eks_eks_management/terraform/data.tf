f4f8e11b9a9ebc88406f891ffbabd179cc41d329
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-17 09:30:41 -0600

    initial push of eks cluster

--- eks_eks_management/terraform/data.tf
+++ eks_eks_management/terraform/data.tf
@@ -14,6 +14,22 @@ data "aws_subnet" "default_vpc_public_subnet_1" {
   }
 }
 
+# data block to get private subnet id for use in creating other assets
+data "aws_subnet" "default_vpc_private_subnet_2" {
+  filter {
+    name   = "tag:Name"
+    values = ["default_vpc_private_subnet_2"]
+  }
+}
+
+# data block to get public subnet id for use in creating other assets
+data "aws_subnet" "default_vpc_public_subnet_2" {
+  filter {
+    name   = "tag:Name"
+    values = ["default_vpc_public_subnet_2"]
+  }
+}
+
 # Data block to get the prod eks security group id by name
 data "aws_security_group" "prod_eks_security_group" {
   filter {
