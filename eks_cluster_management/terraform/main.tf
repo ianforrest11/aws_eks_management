3b76500e2358c4e367598a43ad86174e174b3054
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-20 15:40:06 -0600

    pushing reconfigured templates

--- eks_cluster_management/terraform/main.tf
+++ eks_cluster_management/terraform/main.tf
@@ -2,7 +2,7 @@ module "eks_cluster_creation" {
   source                      = "git@github.com:ianforrest11/aws_terraform_eks_templates.git//eks_cluster?ref=main"
   name                        = var.name
   role_arn                    = data.aws_iam_role.eks_production_role.arn
-  version                     = var.eks_version
+  eks_version                 = var.eks_version
   subnet_ids                  = [
     data.aws_subnet.default_vpc_public_subnet_1.id,
     data.aws_subnet.default_vpc_public_subnet_2.id,
