6b63005d5dd7b30956531261299e6197cbb5aa5b
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-07-28 14:08:40 -0600

    fixing data reference for node group on autoscaling group tagger

--- eks_autoscaling_group_tag_management/terraform/main.tf
+++ eks_autoscaling_group_tag_management/terraform/main.tf
@@ -1,4 +1,4 @@
 module "eks_autoscaling_group_tag_creation" {
   source                      = "git@github.com:ianforrest11/aws_terraform_eks_templates.git//eks_autoscaling_group_tag?ref=main"
-  node_group_name             = data.production_eks_node_group.production_eks_node_group.name
+  node_group_name             = data.eks_node_group.production_eks_node_group.name
 }
